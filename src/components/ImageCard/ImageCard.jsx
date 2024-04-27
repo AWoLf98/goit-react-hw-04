3f4b9296721c06720b27a1497b7896930f2b00e4
Wolf <andresistuk@gmail.com>
2024-04-27 10:56:59 +0300

    change img

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -3,13 +3,16 @@ import css from './ImageCard.module.css';
 const ImageCard = ({ urls, user: { username }, likes, description, openModal }) => {
   const alt = `${description}, photo by ${username}`;
   return (
-    <div
-      className={css.conteiner}
-      onClick={() => {
-        openModal(urls.regular, alt);
-      }}
-    >
-      <img className={css.img} src={urls.small} alt={alt} loading='lazzy'/>
+    <div className={css.conteiner}>
+      <img
+        className={css.img}
+        src={urls.small}
+        alt={alt}
+        onClick={() => {
+          openModal(urls.regular, alt);
+        }}
+        loading="lazzy"
+      />
       <div className={css['img-card']}>
         <span>Likes: {likes} </span>
         <span>User: {username}</span>
